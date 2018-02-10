dc85843dad01e57561e10f4a72023c8572be70e9
izlotnik <ilya@zlotnik.ru>
2018-02-09 22:31:40 -0800

    initial commit with a tutorial scrapy spider

---
+++ tutorial/tutorial/spiders/quotes_spider.py
@@ -0,0 +1,69 @@
+import scrapy
+
+
+# class QuotesSpider(scrapy.Spider):
+#     name = "quotes"
+#
+#     def start_requests(self):
+#         urls = [
+#             'http://quotes.toscrape.com/page/1/',
+#             'http://quotes.toscrape.com/page/2/',
+#         ]
+#         for url in urls:
+#             yield scrapy.Request(url=url, callback=self.parse)
+#
+#     def parse(self, response):
+#         page = response.url.split("/")[-2]
+#         filename = 'quotes-%s.html' % page
+#         with open(filename, 'wb') as f:
+#             f.write(response.body)
+#         self.log('Saved file %s' % filename)
+#
+
+# class QuotesSpider(scrapy.Spider):
+#     name = "quotes"
+#     start_urls = [
+#         'http://quotes.toscrape.com/page/1/',
+#         'http://quotes.toscrape.com/page/2/',
+#     ]
+#
+#     def parse(self, response):
+#         page = response.url.split("/")[-2]
+#         filename = 'quotes-%s.html' % page
+#         with open(filename, 'wb') as f:
+#             f.write(response.body)
+
+# class QuotesSpider(scrapy.Spider):
+#     name = "quotes"
+#     start_urls = [
+#         'http://quotes.toscrape.com/page/1/',
+#         'http://quotes.toscrape.com/page/2/',
+#     ]
+#
+#     def parse(self, response):
+#         for quote in response.css('div.quote'):
+#             yield {
+#                 'text': quote.css('span.text::text').extract_first(),
+#                 'author': quote.css('small.author::text').extract_first(),
+#                 'tags': quote.css('div.tags a.tag::text').extract(),
+#             }
+
+
+class QuotesSpider(scrapy.Spider):
+    name = "quotes"
+    start_urls = [
+        'http://quotes.toscrape.com/page/1/',
+    ]
+
+    def parse(self, response):
+        for quote in response.css('div.quote'):
+            yield {
+                'text': quote.css('span.text::text').extract_first(),
+                'author': quote.css('small.author::text').extract_first(),
+                'tags': quote.css('div.tags a.tag::text').extract(),
+            }
+
+        next_page = response.css('li.next a::attr(href)').extract_first()
+        if next_page is not None:
+            next_page = response.urljoin(next_page)
+            yield scrapy.Request(next_page, callback=self.parse)
